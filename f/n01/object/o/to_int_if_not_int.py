7a704ba52f234db7a49b0585b7364102f1d29d15
John Forbes <john.robert.forbes@gmail.com>
2024-11-26 09:27:12 +1100

    Added ./f/n01/object/o/to_int_if_not_int.py.

---
+++ f/n01/object/o/to_int_if_not_int.py
@@ -0,0 +1,7 @@
+def f(o: object):
+  from f.n02.type_object.c_o.to_k_if_not_k import f as to_k_if_not_k
+  return to_k_if_not_k(c=int, o=o)
+
+def t():
+  from f.n03.object_object_callable.x_y_f.pxyf import f as pxyf
+  return pxyf('123', 123, f)
